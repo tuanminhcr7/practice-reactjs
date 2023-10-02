bfc27beabaffda067c041f123678ab0cea60e508
unknown <minhvnt.hn@havaz.com.vn>
2023-10-02 15:48:40 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/Header';
+import TableUsers from './components/TableUsers';
+import Container from 'react-bootstrap/Container';
+import { Row } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Hoi Dan IT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+
+      <Header />
+      <TableUsers />
+
     </div>
   );
 }
---
+++ src/components/TableUsers.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TableUsers = (props) => {
+    return (
+        <>TableUsers...</>
+    );
+};
+
+export default TableUsers;
